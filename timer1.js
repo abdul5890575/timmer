9cd2a45d3e593fd280331cd6747cfa4459aad320
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-09 19:17:06 +0000

    updated

--- timer1.js
+++ timer1.js
@@ -1,9 +1,19 @@
 let args = process.argv;
 let slicedargs = args.slice(2,args.lenght);
-console.log(slicedargs)
+
+if(slicedargs.length === 0){
+    console.log('no arguments provided')
+}
 
 for(let i = 0; i < slicedargs.length; i++) {
     time = slicedargs[i] *1000
+    if( slicedargs[i] < 0){
+        console.log('cant put negative time')
+        break
+    } else if(isNaN(slicedargs[i]) === true){
+        console.log('time must be in numbers')
+        break
+    }
     setTimeout(() => {
         console.log(slicedargs[i])
         process.stdout.write('\x07');
